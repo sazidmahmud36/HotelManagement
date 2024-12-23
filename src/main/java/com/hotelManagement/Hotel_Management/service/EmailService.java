c1bca3360541301c5b984aa8667edd4236ab073c
Sazid Mahmud <sazidmahmud36@gmail.com>
2024-12-24 01:07:23 +0600

    applicationProperties-update

--- src/main/java/com/hotelManagement/Hotel_Management/service/EmailService.java
+++ src/main/java/com/hotelManagement/Hotel_Management/service/EmailService.java
@@ -4,7 +4,9 @@ import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
@@ -18,7 +20,7 @@ public class EmailService {
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
         helper.setTo(to);
         helper.setSubject(subject);
-        helper.setText(body);
+        helper.setText(body,true);
 
         javaMailSender.send(message);
 
